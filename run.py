31c43892d257e533f4fa08b82c3a5d5a222142ad
Lamp <ndpp2033@gmail.com>
2026-03-25 04:57:48 +0700

    feat: custom button login google

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    # Render cung cấp PORT qua environment variable
-    port = int(os.environ.get('PORT', 5000))
+    # Fly.io yêu cầu port 8080, Render dùng $PORT
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
